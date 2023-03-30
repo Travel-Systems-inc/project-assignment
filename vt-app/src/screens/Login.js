2bb715099d574832d5ae0b81ac2e18c3797a89ac
Yonathan Torres <70590429+YonYon2@users.noreply.github.com>
2023-03-30 06:10:37 +0000

    Installed react-bootstrap and bootstrap is working

--- vt-app/src/screens/Login.js
+++ vt-app/src/screens/Login.js
@@ -1,10 +1,11 @@
 import React from "react"
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Login(){
     return (
         <div className="container">
             <form>
-                <div class="form-group">
+                <div class="form-group w-25 mx-auto">
                     <label for="exampleInputEmail1">Email address</label>
                     <input
                         type="email"
@@ -17,13 +18,9 @@ export default function Login(){
                         We'll never share your email with anyone else.
                     </small>
                 </div>
-                <div class="form-group">
+                <div class="form-group w-25 mx-auto">
                     <label for="exampleInputPassword1">Password</label>
                     <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
-                </div>
-                <div class="form-group form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
-                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
                 </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
             </form>
