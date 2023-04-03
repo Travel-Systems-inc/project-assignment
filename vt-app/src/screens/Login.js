cc940c4eb7ebeb21fa1fdf9a3d9c604af4fd761a
Yonathan Torres <70590429+YonYon2@users.noreply.github.com>
2023-04-03 18:00:37 +0000

    installed bootstrap-icons

--- vt-app/src/screens/Login.js
+++ vt-app/src/screens/Login.js
@@ -3,7 +3,6 @@ import useState from "react"
 import useNavigate from "react-router-dom"
 import signInWithEmailAndPassword from "firebase/auth"
 import auth from "../firebaseSetup"
-import "bootstrap/dist/css/bootstrap.min.css"
 
 export default function Login(){
     const navigate = useNavigate()
